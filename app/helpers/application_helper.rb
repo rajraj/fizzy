de6f782ca3df6bc508994f6fac48b12c174ddfa7
David Heinemeier Hansson <david@37signals.com>
2025-03-29 17:22:51 +0100

    Extract icon_tag helper to reduce repetition

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def page_title_tag
     tag.title @page_title || "Fizzy"
   end
+
+  def icon_tag(name, aria: { hidden: true }, size: 24, **options)
+    image_tag "#{name}.svg", aria: aria, size: size, **options
+  end
 end
